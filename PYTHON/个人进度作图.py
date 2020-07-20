c85dace81c14723b095aa6d364b48d069084642f
xyliaox233 <feng598164096@qq.com>
2020-07-21 01:05:35 +0800

    曲线拟合修改部分拟合函数，高斯函数中点判断，添加图包和json

--- PYTHON/个人进度作图.py
+++ PYTHON/个人进度作图.py
@@ -4,31 +4,47 @@ import json
 from scipy.optimize import curve_fit
 
 
-def cal_error_val(y, yval):
-    error_val = 0
-    for i in range(len(y)):
-        error_val += np.square(y[i] - yval[i])
-    return np.sqrt(error_val)
+def cal_error_val(y, yval, size):
+    sum_y = 0
+    sum_yval = 0
+    sum_y_mul_yval = 0
+    sum_y2 = 0
+    sum_yval2 = 0
+    for i in range(size):
+        sum_y += y[i]
+        sum_yval += yval[i]
+        sum_y_mul_yval += y[i] * yval[i]
+        sum_y2 += np.square(y[i])
+        sum_yval2 += np.square(yval[i])
+
+    div = (sum_y2 - np.square(sum_y) / size) * (sum_yval2 - np.square(sum_yval) / size)
+    # print("除数:",str(div))
+    if div <= 0:
+        r = 0
+    else:
+        r = (sum_y_mul_yval - sum_y * sum_yval / size) / np.sqrt(
+            (sum_y2 - np.square(sum_y) / size) * (sum_yval2 - np.square(sum_yval) / size))
+    return 1 - np.abs(r)
 
 
 # 高斯函数
-def func1(x, a, b, c):
-    return a * np.power(np.e, -np.square(x - b) / 2 * np.square(c))
+def func1(x, a, b, c, d):
+    return a * np.power(np.e, -np.square(x - b) / 2 * np.square(c)) + d
 
 
-# 幂函数
-def func2(x, a, b):
-    return a * np.power(x, b)
+# 幂函数 x
+def func2(x, a, b, c):
+    return a * np.power(x, b) + c
 
 
 # 对数函数
-def func3(x, a, b):
-    return a * np.log(x, b)
+def func3(x, a, b, c):
+    return a * np.log(x, b) + c
 
 
-# S型函数
-def func4(x, a, b):
-    return 1 / (a + b * np.power(np.e, -x))
+# S型函数 ?
+def func4(x, a, b, c):
+    return 1 / (a + b * np.power(np.e, -x)) + c
 
 
 # sin
@@ -39,6 +55,11 @@ def func5(x, a, b, c, d):
 # 指数函数
 def func6(x, a, b, c):
     return a * b ** x + c
+
+
+# a/x
+def func7(x, a, b, c):
+    return a / np.power(x, b) + c
 
 
 def x_n(x, y, n):
@@ -58,130 +79,164 @@ def x_n(x, y, n):
 
 if __name__ == '__main__':
 
+    dic = {
+    }
+
     # 用于拟合的函数
-    func_list = [func1, func2, func3, func4, func5, func6]
+    func_list = [func1, func3, func5, func6, func7]
 
-    for user_id in range(0,53):  # 输入要查看的userId
-        # 记录最小标准差选择拟合函数
-        min_error_val = None
-        # 最终确认的拟合曲线
-        y_fit_val = []
+    for user_id in range(46):  # 输入要查看的userId
 
-        # 拟合曲线类型
-        # 三次函数形状问题？找拐点
-        fit_type = -1
-        type_list = ["高斯函数", "幂函数", "对数函数", "S型函数", "sin", "指数函数", "一次增函数", "一次减函数", "U型二次函数", "n型二次函数"]
+        type_list = ["高斯函数", "对数函数", "正弦函数", "上升指数函数", "下降指数函数", "反比例函数", "一次增函数", "一次减函数", "U型二次函数", "n型二次函数"]
 
         f = open('personal_progress_data.json', encoding='utf-8')
         res = f.read()
         user_data = json.loads(res)
 
-        period = []
-        dataC = []
+        # 选择拟合函数
+        min_error_val = None
+        # 最终确认的拟合曲线
+        y_fit_val = []
+        x_fit_val = []
+        fit_dataC = []
+        fit_popt=[]
 
-        # 测试的数据记录：
-        # user[2]:标准高斯函数
-        # user[7]:高斯函数只有下降的一半
-        # user[30]：应该是指数函数 还没补上去，我去恰饭啦，耶
-        # user[31 32]:a>0的三次函数，无拐点
-        # USER[25]:a>0的三次函数但是只有形如二次函数的前一半
+        # 拟合曲线类型
+        # 三次函数形状问题？找拐点
+        fit_type = -1
 
-        user = user_data[user_id]
-        dailyP = user['daily_progress']
-        set_period_length = 3  # 设置几天为一个period
-        cnt = 0
-        p = 1
-        period_comp = 0
-        # 注意：补全日期数据保证period数相同
-        for item in dailyP:
-            if cnt == set_period_length:
+        fit_period_length = 0
+
+        for set_period_length in [1, 2, 3]:  # 设置几天为一个period
+
+            period = []
+            dataC = []
+
+            # 测试的数据记录：
+            # user[2]:标准高斯函数
+            # user[7]:高斯函数只有下降的一半
+            # user[30]：应该是指数函数 还没补上去，我去恰饭啦，耶
+            # user[31 32]:a>0的三次函数，无拐点
+            # USER[25]:a>0的三次函数但是只有形如二次函数的前一半
+
+            user = user_data[user_id]
+            dailyP = user['daily_progress']
+
+            cnt = 0
+            p = 1
+            period_comp = 0
+            # 注意：补全日期数据保证period数相同
+            for item in dailyP:
+                if cnt == set_period_length:
+                    period.append(p)
+                    dataC.append(period_comp / 500)
+                    period_comp = 0
+                    cnt = 0
+                    p += 1
+                period_comp += item['completion']
+                cnt += 1
+            if cnt != 0:
                 period.append(p)
                 dataC.append(period_comp / 500)
-                period_comp = 0
-                cnt = 0
-                p += 1
-            period_comp += item['completion']
-            cnt += 1
-        if cnt != 0:
-            period.append(p)
-            dataC.append(period_comp / 500)
-            # 注意：为了判断周期把纵坐标缩短了
+                # 注意：为了判断周期把纵坐标缩短了
 
-        print(period)
-        print(dataC)
+            print(period)
+            print(dataC)
 
-        x = np.array(period)
-        y = np.array(dataC)
+            x = np.array(period)
+            y = np.array(dataC)
 
-        type = 0
-        # 记录拟合函数类型，先遍历拟合func_list，再遍历多项式次数
+            type = 0
+            # 记录拟合函数类型，先遍历拟合func_list，再遍历多项式次数
 
-        # 非线性最小二乘法拟合
+            # 非线性最小二乘法拟合
 
-        for func in func_list:
-            try:
-                popt, pcov = curve_fit(func, x, y)
-                cf = func
-                # 获取popt里面是拟合系数
-                # print(popt)
-                yvals = func(x, *popt)  # 拟合y值
-                # print('popt:', popt)
-                er_val = cal_error_val(y, yvals)
+            for func in func_list:
+                try:
+                    popt, pcov = curve_fit(func, x, y)
+                    cf = func
+                    # 获取popt里面是拟合系数
+                    # print(popt)
+                    yvals = func(x, *popt)  # 拟合y值
+                    # print('popt:', popt)
+                    er_val = cal_error_val(y, yvals, len(x))
+                    # 判断是否为最佳拟合
+                    if min_error_val is None or er_val < min_error_val:
+                        if type == 0:  # 高斯函数期望要求
+                            if popt[1] <= 2 or popt[1] >= len(x):
+                                type += 1
+                                continue
+                        if type == 2:  # sin周期要求
+                            T = np.abs(2 * np.pi / popt[1])
+                            if len(x) <= 1.5 * T:
+                                type += 1
+                                continue
+                        min_error_val = er_val
+                        fit_type = type
+                        if type == 3:  # 指数函数趋势判断
+                            if popt[1] < 1: fit_type += 1
+                        y_fit_val = yvals
+                        x_fit_val = x
+                        fit_dataC = dataC
+                        fit_popt=popt
+                        fit_period_length = set_period_length
+                except:
+                    pass
+                if type == 3:
+                    type += 2
+                else:
+                    type += 1
+
+            print(type)
+            # 遍历拟合多项式，n为多项式次数
+            for n in [1, 2]:
+                res = x_n(x, y, n)
+                er_val = cal_error_val(y, res["yvals"], len(x))
+                yvals = res["yvals"]
                 # 判断是否为最佳拟合
                 if min_error_val is None or er_val < min_error_val:
-                    if type == 4:
-                        T = np.abs(2 * np.pi / popt[1])
-                        print(popt[1], T)
-                        if len(x) <= 2 * T:
-                            type += 1
+                    if n == 2:
+                        a = res["系数"][0]
+                        b = res["系数"][1]
+                        T = -b / (2 * a)  # 求出拐点，若拐点不在图像显示区间内则抛弃
+                        if T <= 2 or len(x) * 0.9 <= T:
+                            type += 2
                             continue
                     min_error_val = er_val
-                    fit_type = type
                     y_fit_val = yvals
-            except:
-                pass
-            type += 1
-
-        # 遍历拟合多项式，n为多项式次数
-        for n in [1, 2]:
-            res = x_n(x, y, n)
-            er_val = cal_error_val(y, res["yvals"])
-            yvals = res["yvals"]
-            # 判断是否为最佳拟合
-            if min_error_val is None or er_val < min_error_val:
-                if n == 2:
-                    a = res["系数"][0]
-                    b = res["系数"][1]
-                    T=-b/(2*a)  # 求出拐点，若拐点不在图像显示区间内则抛弃
-                    print(popt[1], T)
-                    if T<=0 or len(x) <= T:
-                        type += 1
-                        continue
-                min_error_val = er_val
-                y_fit_val = yvals
-                if n == 2:
-                    if res["系数"][0] > 0:  # 判断a后直接给type赋值
-                        type = 8
+                    x_fit_val = x
+                    fit_dataC = dataC
+                    fit_popt=res["系数"]
+                    fit_period_length = set_period_length
+                    if res["系数"][0] > 0:
+                        fit_type = type
                     else:
-                        type = 9
-                if yvals[0] > 0:
-                    fit_type = type
-                else:
-                    fit_type = type
+                        fit_type = type + 1
 
-        # 2:u,n
-        # 3:/\/ \/\
+                type += 2
+
+            # 2:u,n
+            # 3:/\/ \/\
 
         # 绘图
-
+        print("R：", 1 - min_error_val)
         print("拟合函数：", type_list[fit_type])
+        print("天数：", fit_period_length)
 
-        plt.plot(period, dataC)
-        plt.plot(x, y_fit_val, 'r', label='polyfit values')
+        plt.plot(x_fit_val, fit_dataC)
+        plt.plot(x_fit_val, y_fit_val, 'r', label='polyfit values')
 
-        plt.title("userId: " + user['user_id'], fontsize=20)
+        plt.title("userId: " + user['user_id'] + "  R=" + str(1 - min_error_val)[0:6], fontsize=15)
         plt.xlabel("period", fontsize=12)
         plt.ylabel("completion", fontsize=12)
         plt.tick_params(axis='both', labelsize=10)
-        plt.savefig('I:/big_code_backup/3天拟合图/' + user['user_id'] +'_3' +'.png')  # 保存图片
+    #    plt.savefig('D:/bigcode/master/pics/最匹配拟合图/' + user['user_id'] + '.png')  # 保存图片
         plt.show()
+
+        dic[user['user_id']]={
+            "拟合类型":fit_type,
+            "拟合函数":type_list[fit_type],
+            "相关系数R":1 - min_error_val
+        }
+    # with open('D:/bigcode/master/JSON/得分趋势拟合信息.json', 'a', encoding='utf8') as fp1:
+    #     json.dump(dic, fp1, ensure_ascii=False)
